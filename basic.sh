a552893e7317b2c2a1378396e17e0fc7707567ed
root <root@localhost.localdomain>
2022-01-25 19:59:00 +0530

    first commit

---
+++ basic.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+for i in $(cat db.csv)
+do 
+    echo "user created, $i at time and date $(date)"
+done
+
+if [ "Hi" == "Hi" ]
+then
+	echo "This is my work from home"
+fi
---
+++ f.py
@@ -0,0 +1,22 @@
+Disk /dev/sda: 50.6 GiB, 54331336192 bytes, 106115891 sectors
+Units: sectors of 1 * 512 = 512 bytes
+Sector size (logical/physical): 512 bytes / 512 bytes
+I/O size (minimum/optimal): 512 bytes / 512 bytes
+Disklabel type: dos
+Disk identifier: 0x768766b0
+
+Device     Boot   Start       End   Sectors  Size Id Type
+/dev/sda1  *       2048   2099199   2097152    1G 83 Linux
+/dev/sda2       2099200 106115071 104015872 49.6G 8e Linux LVM
+
+
+Disk /dev/mapper/rhel-root: 47.5 GiB, 51032096768 bytes, 99672064 sectors
+Units: sectors of 1 * 512 = 512 bytes
+Sector size (logical/physical): 512 bytes / 512 bytes
+I/O size (minimum/optimal): 512 bytes / 512 bytes
+
+
+Disk /dev/mapper/rhel-swap: 2.1 GiB, 2222981120 bytes, 4341760 sectors
+Units: sectors of 1 * 512 = 512 bytes
+Sector size (logical/physical): 512 bytes / 512 bytes
+I/O size (minimum/optimal): 512 bytes / 512 bytes
